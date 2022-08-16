6bc96950a31ef645d4635f70d238bc3689208ab1
root <root@ip-172-31-9-65.us-west-2.compute.internal>
2022-08-16 20:24:59 +0000

    git tutorial commit

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import traceback
 from datetime import datetime
 from http import HTTPStatus
 
+#This is git tutorial
+
 from aiohttp import web
 from aiohttp.web import Request, Response, json_response
 from botbuilder.ai.qna.dialogs import QnAMakerDialog
