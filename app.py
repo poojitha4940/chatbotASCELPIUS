e127b8c127bfe6a697976393cd968638fa5aa090
root <root@ip-172-31-1-55.us-west-2.compute.internal>
2022-08-16 20:42:43 +0000

    This commit is done by second developer

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from http import HTTPStatus
 
 #This is git tutorial
-
+#This is second developer
 from aiohttp import web
 from aiohttp.web import Request, Response, json_response
 from botbuilder.ai.qna.dialogs import QnAMakerDialog
