990e4f4424bbf4adce6aebadc39c5619def297eb
root <root@ip-172-31-24-249.us-west-2.compute.internal>
2023-03-19 19:19:30 +0000

    added new file and edited app.py

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ import traceback
 from datetime import datetime
 from http import HTTPStatus
 
-#This is git tutorial
-#This is second developer
-#This is first developer
-#git branching session
+#This is second git tutorial
 from aiohttp import web
 from aiohttp.web import Request, Response, json_response
 from botbuilder.ai.qna.dialogs import QnAMakerDialog
