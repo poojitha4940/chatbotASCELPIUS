c62198d16af093b5eae360e8b1de22d9a0f1c156
root <root@ip-172-31-26-190.us-west-2.compute.internal>
2023-03-31 19:44:08 +0000

    added a comment to app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from http import HTTPStatus
 
 #This is git tutorial
+#This is second tutorial in github 
 from aiohttp import web
 from aiohttp.web import Request, Response, json_response
 from botbuilder.ai.qna.dialogs import QnAMakerDialog
